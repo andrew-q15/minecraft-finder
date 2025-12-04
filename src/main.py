9b9ecd9d3675629f7f3dae805a763bc8c7eba8d3
yichen <yichen.qu@outlook.com>
2025-12-04 21:50:55 +0800

    feat: enhance main function to allow continuous querying until user opts to exit

--- src/main.py
+++ src/main.py
@@ -2,17 +2,21 @@ import requests
 
 
 def main() -> None:
-    query = input("请输入你要查询的词: ")
-    print(f"正在查询: {query} ...")
-    print("================================================================")
-    response = requests.get(
-        f"https://minecraft.wiki/rest.php/v1/search/page?format=json&limit=5&q={query}"
-    )
-    data = response.json()
-    dataList = data.get("pages", [])
-    for item in dataList:
-        print("%-10s %s" % (item.get("id", ""), item.get("title", "")))
-        print("____________________________________________________________")
+    while True:
+        query = input("请输入你要查询的词或输入over以结束: ")
+        if query == "over":
+            print("谢谢使用,正在结束...")
+            break
+        print(f"正在查询: {query} ...")
+        print("================================================================")
+        response = requests.get(
+            f"https://minecraft.wiki/rest.php/v1/search/page?format=json&limit=5&q={query}"
+        )
+        data = response.json()
+        dataList = data.get("pages", [])
+        for item in dataList:
+            print("%-10s %s" % (item.get("id", ""), item.get("title", "")))
+            print("____________________________________________________________")
 
 
 if __name__ == "__main__":
