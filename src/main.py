f562b20eba540936054cc3e81ffe00327460864c
yichen <yichen.qu@outlook.com>
2025-12-04 21:37:07 +0800

    fix: correct indentation and improve query handling in main function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
+import requests
+
+
 def main() -> None:
     query = input("请输入你要查询的词: ")
     print(f"正在查询: {query} ...")
+    print("================================================================")
+    response = requests.get(
+        f"https://minecraft.wiki/rest.php/v1/search/page?format=json&limit=5&q={query}"
+    )
+    data = response.json()
+    dataList = data.get("pages", [])
+    for item in dataList:
+        print("%-10s %s" % (item.get("id", ""), item.get("title", "")))
+        print("____________________________________________________________")
 
 
 if __name__ == "__main__":
