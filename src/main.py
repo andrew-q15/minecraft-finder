75f7bbf720a4f0ff49cf9a3f561c3190aea9dfd3
yichen <yichen.qu@outlook.com>
2025-12-09 21:50:51 +0800

    feat: add argparse to main.py for improved command-line argument handling

--- src/main.py
+++ src/main.py
@@ -1,21 +1,40 @@
+import argparse
+
 from search import searchFromWiki
 
 
+def searchWithOutput(query: str):
+    print(f"正在查询: {query} ...")
+    print("================================================================")
+
+    dataList = searchFromWiki(query)
+
+    for item in dataList:
+        print("%-10s %s" % (item.get("id", ""), item.get("title", "")))
+        print("____________________________________________________________")
+
+
 def main() -> None:
-    while True:
-        query = input("请输入你要查询的词或输入over以结束: ")
-        if query == "over":
-            print("谢谢使用,正在结束...")
-            break
+    parser = argparse.ArgumentParser(description="Search anything in minecraft")
+    parser.add_argument(
+        "--query",
+        "-q",
+        type=str,
+        help="Your query.",
+    )
+    args = parser.parse_args()
 
-        print(f"正在查询: {query} ...")
-        print("================================================================")
+    if not args.query:
+        while True:
+            query = input("请输入你要查询的词或输入 over 以结束: ")
+            if query == "over":
+                print("谢谢使用,正在结束...")
+                break
 
-        dataList = searchFromWiki(query)
-
-        for item in dataList:
-            print("%-10s %s" % (item.get("id", ""), item.get("title", "")))
-            print("____________________________________________________________")
+            searchWithOutput(query)
+    else:
+        query = args.query
+        searchWithOutput(query)
 
 
 if __name__ == "__main__":
